c04440e530583d159dbb2eca1146a8c414580786
kuangxj <frankxjkuang@gmail.com>
2022-07-22 09:48:35 +0800

    🎸feat: new package string

---
+++ string/string.go
@@ -0,0 +1,15 @@
+/**
+ * @Time : 2022/7/22 9:47 上午
+ * @Author : frankj
+ * @Email : --
+ * @Description : --
+ * @Revise : --
+ */
+
+package string
+
+import "fmt"
+
+func init()  {
+	fmt.Println("hello string")
+}
